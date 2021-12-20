efeaea862a6c02ecc28bb330e7f84cd5bc2fcc2f
Ford Burgess <76268861+fordburgess@users.noreply.github.com>
2021-12-20 16:19:09 -0600

    Add files via upload
    useContext sort of works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import "./App.css";
 import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import CorrectLetters from "./components/CorrectLetters";
 import Popup from "./components/Popup";
-import { checkWin } from "./helpers";
 
 const words = ["pakistan", "abu dhabi", "edinburgh", "washington"];
 const word = words[Math.floor(Math.random() * words.length)];
 const selectedWord = word.toUpperCase();
+export const ThemeContext = React.createContext();
 
 function App() {
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
   const [playable, setPlayable] = useState(true);
+  //
+  const [darkTheme, setDarkTheme] = useState(false);
+  const themeStyles = {
+    backgroundColor: darkTheme ? "rgb(90, 60, 20)" : "tan",
+    color: darkTheme ? "tan" : "rgb(90, 60, 20",
+  };
+  //
   const errors = wrongLetters.length;
 
   useEffect(() => {
+    console.log(selectedWord);
     const eventHandler = (e) => {
       var { key, keyCode } = e;
       if (playable === true && keyCode >= 65 && keyCode <= 90) {
@@ -41,26 +49,38 @@ function App() {
     return () => window.removeEventListener("keydown", eventHandler);
   }, [correctLetters, wrongLetters, playable]);
 
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+    console.log(darkTheme);
+  }
+
   return (
-    <div className="game-container">
-      <h1 className="header">Geography Hangman</h1>
-      <div className="middle-container">
-        <div className="middle-sub-container">
-          <Figure errors={errors} />
-          <WrongLetters wrongLetters={wrongLetters} />
+    <ThemeContext.Provider value={themeStyles}>
+      <div className="body">
+        <button className="theme-toggle" onClick={toggleTheme}>
+          Change Theme
+        </button>
+        <div className="game-container">
+          <h1 className="header">Geography Hangman</h1>
+          <div className="middle-container">
+            <div className="middle-sub-container">
+              <Figure errors={errors} />
+              <WrongLetters wrongLetters={wrongLetters} />
+            </div>
+            <CorrectLetters
+              selectedWord={selectedWord}
+              correctLetters={correctLetters}
+            />
+          </div>
+          <Popup
+            correctLetters={correctLetters}
+            errors={errors}
+            selectedWord={selectedWord}
+            setPlayable={setPlayable}
+          />
         </div>
-        <CorrectLetters
-          selectedWord={selectedWord}
-          correctLetters={correctLetters}
-        />
       </div>
-      <Popup
-        correctLetters={correctLetters}
-        errors={errors}
-        selectedWord={selectedWord}
-        setPlayable={setPlayable}
-      />
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
