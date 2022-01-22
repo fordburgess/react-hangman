3671f277d7d5bfa3c91558f15152ef1a01f525ae
Ford Burgess <76268861+fordburgess@users.noreply.github.com>
2022-01-22 19:04:16 +0000

    Add files via upload
    Fixed Dark Theme not working
    App can now handle words with non alphabet characters

--- App.js
+++ App.js
@@ -4,23 +4,41 @@ import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import CorrectLetters from "./components/CorrectLetters";
 import Popup from "./components/Popup";
-import { checkWin } from "./helpers";
-import { countries } from "./helpers";
+import COUNTRIES from "./countries"
 
-const words = [];
-countries.forEach((country) => {
-  words.push(country.name);
-});
-var selectedWord = words[Math.floor(Math.random() * words.length)];
-selectedWord = selectedWord.toUpperCase();
+const word = COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)];
+const selectedWord = word.toUpperCase();
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  //
+  useEffect(() => {
+    fetch("http://battuta.medunes.net/api/country/all/?key=76ebe15227b3b9e9c698a4295e9a58a1")
+    .then(result => result.json()).then(data => console.log(data))
+  }, [])
+  //
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
   const [playable, setPlayable] = useState(true);
-  const errors = wrongLetters.length;
+  /* CONTEXT HANDLING */
+  const [darkTheme, setDarkTheme] = useState(false);
+  const themeStyles = {
+    backgroundColor: darkTheme ? "rgb(90, 60, 20)" : "tan",
+    color: darkTheme ? "tan" : "rgb(90, 60, 20)",
+    stroke: darkTheme ? "tan" : "rgb(90, 60, 20)",
+  };
 
+  const errors = wrongLetters.length;
   useEffect(() => {
+    /* ACTUAL FUNCTIONALITY */
+    // selectedWord.split('').forEach(character => {
+    //   if (!/[A-Z]/.test(character)) {
+    //     setCorrectLetters((currentLetters) => [...currentLetters, character])
+    //     console.log(correctLetters)
+    //   }
+    // })
+
     const eventHandler = (e) => {
       var { key, keyCode } = e;
       if (playable === true && keyCode >= 65 && keyCode <= 90) {
@@ -45,26 +63,44 @@ function App() {
     return () => window.removeEventListener("keydown", eventHandler);
   }, [correctLetters, wrongLetters, playable]);
 
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+
   return (
-    <div className="game-container">
-      <h1 className="header">Geography Hangman</h1>
-      <div className="middle-container">
-        <div className="middle-sub-container">
-          <Figure errors={errors} />
-          <WrongLetters wrongLetters={wrongLetters} />
+    <ThemeContext.Provider value={themeStyles}>
+      <div className="body" style={themeStyles}>
+        <button
+          className="theme-toggle"
+          onClick={toggleTheme}
+          style={{
+            background: darkTheme ? "tan" : "rgb(90, 60, 20)",
+            color: darkTheme ? "rgb(90, 60, 20)" : "tan",
+          }}
+        >
+          Change Theme
+        </button>
+        <div className="game-container">
+          <h1 className="header">Geography Hangman</h1>
+          <div className="middle-container">
+            <div className="middle-sub-container">
+              <Figure errors={errors} darkTheme={darkTheme}/>
+              <WrongLetters wrongLetters={wrongLetters} />
+            </div>
+            <CorrectLetters
+              selectedWord={selectedWord}
+              correctLetters={correctLetters}
+            />
+          </div>
+          <Popup
+            correctLetters={correctLetters}
+            errors={errors}
+            selectedWord={selectedWord}
+            setPlayable={setPlayable}
+          />
         </div>
-        <CorrectLetters
-          selectedWord={selectedWord}
-          correctLetters={correctLetters}
-        />
       </div>
-      <Popup
-        correctLetters={correctLetters}
-        errors={errors}
-        selectedWord={selectedWord}
-        setPlayable={setPlayable}
-      />
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
